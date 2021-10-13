12b68153bce9a289658e60b48810db342a4c0255
nonowelle <menoel888@gmail.com>
2021-10-13 19:49:54 -0400

    add all of api charlie benoit previously created

---
+++ routes/confirmations.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/", (req, res, next) => {
+  let config = {
+    method: "get",
+    url: "https://confirmations-1a40.restdb.io/rest/invites",
+    headers: {
+      "x-apikey": process.env.API_KEY,
+      "Content-Type": "application/json",
+    },
+  };
+
+  axios(config)
+    .then((response) => {
+      console.log("IN THE RESPONSE");
+
+      res.send(response.data);
+
+      res.send(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error.message);
+    });
+});
+
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+  const answer = {
+    lastName: req.body.lastName,
+    firstName: req.body.firstName,
+    email: req.body.email,
+    phone: req.body.phone,
+    answer: req.body.response,
+  };
+  console.log(answer);
+  var request = require("request");
+
+  var options = {
+    method: "POST",
+    url: "https://confirmations-1a40.restdb.io/rest/invites",
+    headers: {
+      "cache-control": "no-cache",
+      "x-apikey": "1cafe281210a9ab5837d477312051f4143e0c",
+      "content-type": "application/json",
+    },
+    body: answer,
+    json: true,
+  };
+
+  request(options, function (error, response, body) {
+    if (error) {
+      throw new Error(error);
+    } else {
+      console.log(response.body);
+      res.send(JSON.stringify(response.body));
+    }
+  });
+});
+
+module.exports = router;
---
+++ routes/router.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+/**
+ * HTTP Status Codes: https://developer.mozilla.org/de/docs/Web/HTTP/Status
+ */
+
+// log requests
+router.use("*", (req, res, next) => {
+  console.log(`\x1b[32m${req.method}\x1b[35m ${req.baseUrl}`);
+  next();
+});
+
+// add single router files
+const routeFiles = ["confirmations"];
+routeFiles.forEach((route) => {
+  let routeFile = require(`./${route}.js`);
+  try {
+    router.use(`/${route}`, routeFile);
+  } catch (err) {
+    console.log(`\u001b[31m[ERR] Router file ${routeFile} does not exists.`);
+  }
+});
+
+module.exports = router;
