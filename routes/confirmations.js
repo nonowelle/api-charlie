ee5c1c346761a11110666b4d553423d8a399e15f
nonowelle <menoel888@gmail.com>
2021-10-13 21:34:20 -0400

    refactor post request

--- routes/confirmations.js
+++ routes/confirmations.js
@@ -27,7 +27,7 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   console.log(req.body);
   const answer = {
     lastName: req.body.lastName,
@@ -36,29 +36,35 @@ router.post("/", (req, res, next) => {
     phone: req.body.phone,
     answer: req.body.response,
   };
+  const sentToDB = JSON.stringify(answer);
   console.log(answer);
   var request = require("request");
 
   var options = {
     method: "POST",
-    url: "https://confirmations-1a40.restdb.io/rest/invites",
     headers: {
       "cache-control": "no-cache",
       "x-apikey": "1cafe281210a9ab5837d477312051f4143e0c",
       "content-type": "application/json",
     },
-    body: answer,
+    body: sentToDB,
     json: true,
   };
 
-  request(options, function (error, response, body) {
-    if (error) {
-      throw new Error(error);
-    } else {
-      console.log(response.body);
-      res.send(JSON.stringify(response.body));
-    }
-  });
-});
+  fetch("https://confirmations-1a40.restdb.io/rest/invites", options)
+  .then ((response) => response.json())
+  .then((result)=> {
+    console.log(result);
+    res.send(result)
+  })
+  .catch((error) =>console.log("error", error));
+//     if (error) {
+//       throw new Error(error);
+//     } else {
+//       console.log(response.body);
+//       res.send(JSON.stringify(response.body));
+//     }
+//   });
+// });
 
 module.exports = router;
