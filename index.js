3b659a5e06b476d89561c89eeff1f4577ea73723
nonowelle <menoel888@gmail.com>
2021-10-13 22:00:09 -0400

    add set header middleware

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ if (process.env.NODE_ENV !== "production") {
 }
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,content-type, Accept, Authorization"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // add router for all routes
 const router = require("./routes/router.js");
