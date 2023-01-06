e462b48f448f7d9ee6f57fbbad3d9213d7eac03b
Nonowelle <menoel888@gmail.com>
2023-01-06 17:47:57 +0100

    remove api key

--- routes/confirmations.js
+++ routes/confirmations.js
@@ -1,40 +1,38 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const axios = require("axios");
-const bodyParser = require("body-parser");
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const api_key = process.env.API_KEY;
-const cors = require("cors");
+const cors = require('cors');
 
 router.use(bodyParser.json());
 const corsOptions = {
   origin: true,
-  methods: ["POST", "GET", "PATCH", "DELETE", "OPTIONS"],
+  methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "Origin",
-    "Access-Control-Allow-Origin",
-    "Accept",
-    "Options",
+    'Content-Type',
+    'Authorization',
+    'Origin',
+    'Access-Control-Allow-Origin',
+    'Accept',
+    'Options',
   ],
 };
 
-router.get("/", cors(corsOptions), (req, res) => {
+router.get('/', cors(corsOptions), (req, res) => {
   let config = {
-    method: "get",
-    url: "https://confirmations-1a40.restdb.io/rest/invites",
+    method: 'get',
+    url: 'https://confirmations-1a40.restdb.io/rest/invites',
     headers: {
-      "x-apikey": api_key,
-      "Content-Type": "application/json",
+      'x-apikey': api_key,
+      'Content-Type': 'application/json',
     },
   };
 
   axios(config)
     .then((response) => {
-      console.log("IN THE RESPONSE");
-      // res.set("Access-Control-Allow-Origin", "http://localhost:8080");
-
+      console.log('IN THE RESPONSE');
       res.send(response.data);
     })
     .catch(function (error) {
@@ -42,7 +40,7 @@ router.get("/", cors(corsOptions), (req, res) => {
     });
 });
 
-router.post("/", cors(corsOptions), (req, res) => {
+router.post('/', cors(corsOptions), (req, res) => {
   console.log(req.body);
 
   const answer = {
@@ -53,15 +51,15 @@ router.post("/", cors(corsOptions), (req, res) => {
     answer: req.body.answer,
   };
 
-  let request = require("request");
+  let request = require('request');
 
   let options = {
-    method: "POST",
-    url: "https://confirmations-1a40.restdb.io/rest/invites",
+    method: 'POST',
+    url: 'https://confirmations-1a40.restdb.io/rest/invites',
     headers: {
-      "cache-control": "no-cache",
-      "x-apikey": "1cafe281210a9ab5837d477312051f4143e0c",
-      "content-type": "application/json",
+      'cache-control': 'no-cache',
+      'x-apikey': process.env.XAPIKEY,
+      'content-type': 'application/json',
     },
     body: answer,
     json: true,
