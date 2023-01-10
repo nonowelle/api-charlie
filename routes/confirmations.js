c99855f2bff3017d1747c083979d80b918c1fe96
Nonowelle <menoel888@gmail.com>
2023-01-10 19:12:27 +0100

    ai key error

--- routes/confirmations.js
+++ routes/confirmations.js
@@ -25,7 +25,7 @@ router.get('/', cors(corsOptions), (req, res) => {
     method: 'get',
     url: 'https://confirmations-1a40.restdb.io/rest/invites',
     headers: {
-      'x-apikey': process.env.api_key,
+      'x-apikey': process.env.API_KEY,
       'Content-Type': 'application/json',
     },
   };
