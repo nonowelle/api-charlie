c3da4882626cf7a0a55a0be70e301fc759d7557f
Nonowelle <menoel888@gmail.com>
2023-01-10 19:07:10 +0100

    cors

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-// const bodyParser = require("body-parser");
+const cors = require('cors');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors);
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
-// app.use(express.json());
-// app.use(bodyParser.json());
 
 // add router for all routes
-const router = require("./routes/router.js");
-app.use("/api", router);
+const router = require('./routes/router.js');
+app.use('/api', router);
 
 // handle unhandled 404 requests
-app.use("*", (req, res) => {
+app.use('*', (req, res) => {
   console.log(`\u001b[31m[ERR] Route does not exists: ${req.baseUrl}`);
 });
 
